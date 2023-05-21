98aead4f9c6676f3695b13cf956b9b731f74da92
rakirs2 <srmylavarapu@microsoft.com>
2023-05-21 10:07:11 -0500

    fixing run file

--- WrestlingScoreboard.py
+++ WrestlingScoreboard.py
@@ -1,80 +1,152 @@
 import tkinter
 import random
 
-colours = ['Red','Blue','Green','Pink','Black',
-		'Yellow','Orange','White','Purple','Brown']
-score = 0
 
-timeleft = 30
+scores = ['0', '1', '2', '3', '4',
+          '5', '-5', '-4', '-3', '-2', '-1']
+greenscore = 0
+redscore = 0
+
+timeleft = 90
+
 
 def startGame(event):
-	
-	if timeleft == 30:
-		countdown()
-		
-	nextColour()
+
+    if timeleft == 90:
+
+        countdown()
+
+    nextColour()
+
 
 def nextColour():
 
-	global score
-	global timeleft
+    global redscore
+    global greenscore
+    global timeleft
 
-	if timeleft > 0:
-		e.focus_set()
-		
-		if e.get().lower() == colours[1].lower():
-			
-			score += 1
+    if timeleft > 0:
 
-		e.delete(0, tkinter.END)
-		
-		random.shuffle(colours)
+        e.focus_set()
+        x.focus_set()
 
-		label.config(fg = str(colours[1]), text = str(colours[0]))
+        if e.get().lower() == scores[1]:
 
-		scoreLabel.config(text = "Score: " + str(score))
+            greenscore += 1
+        if e.get().lower() == scores[2]:
+            greenscore += 2
+        if e.get().lower() == scores[3]:
+            greenscore += 3
+        if e.get().lower() == scores[4]:
+            greenscore += 4
+        if e.get().lower() == scores[5]:
+            greenscore += 5
+        if e.get().lower() == scores[-1]:
+            greenscore -= 1
+        if e.get().lower() == scores[-2]:
+            greenscore -= 2
+        if e.get().lower() == scores[-3]:
+            greenscore -= 3
+        if e.get().lower() == scores[-4]:
+            greenscore -= 4
+        if e.get().lower() == scores[-5]:
+            greenscore -= 5
+        if x.get().lower() == scores[1]:
+
+            redscore += 1
+        if x.get().lower() == scores[2]:
+            redscore += 2
+        if x.get().lower() == scores[3]:
+            redscore += 3
+        if x.get().lower() == scores[4]:
+            redscore += 4
+        if x.get().lower() == scores[5]:
+             redscore += 5
+        if x.get().lower() == scores[-1]:
+             redscore -= 1
+        if x.get().lower() == scores[-2]:
+             redscore -= 2
+        if x.get().lower() == scores[-3]:
+             redscore -= 3
+        if x.get().lower() == scores[-4]:
+             redscore -= 4
+        if x.get().lower() == scores[-5]:
+             redscore -= 5    
+        e.delete(0, tkinter.END)
+        x.delete(0, tkinter.END)
+
+        greenscoreLabel.config(text="green Score: " + str(greenscore))
+
+        redscoreLabel.config(text="redscore: " + str(redscore))
+
+# Countdown timer function
+
 
 def countdown():
 
-	global timeleft
+    global timeleft
 
-	if timeleft > 0:
+    # if a game is in play
+    if timeleft > 0:
 
-		timeleft -= 1
-		
-		timeLabel.config(text = "Time left: "
-							+ str(timeleft))
-								
-		timeLabel.after(1000, countdown)
+        # decrement the timer.
+        timeleft -= 1
 
+        # update the time left label
+        timeLabel.config(text="Time left: "
+                         + str(timeleft))
+
+        # run the function again after 1 second.
+        timeLabel.after(1000, countdown)
+
+
+# Driver Code
+
+# create a GUI window
 root = tkinter.Tk()
 
+# set the title
 root.title("COLORGAME")
 
-root.geometry("375x200")
+# set the size
+root.geometry("500x800")
 
-instructions = tkinter.Label(root, text = "Type in the colour"
-						"of the words, and not the word text!",
-									font = ('Helvetica', 12))
+# add an instructions label
+instructions = tkinter.Label(root, text="Type in the red and green wrestlers score at inputs",
+                             font=('Helvetica', 12))
 instructions.pack()
-    
-scoreLabel = tkinter.Label(root, text = "Press enter to start",
-									font = ('Helvetica', 12))
-scoreLabel.pack()
 
-timeLabel = tkinter.Label(root, text = "Time left: " +
-			str(timeleft), font = ('Helvetica', 12))
-			
+# add a score label
+greenscoreLabel = tkinter.Label(root, text="Press enter to start",
+                                font=('Helvetica', 12))
+greenscoreLabel.pack()
+
+redscoreLabel = tkinter.Label(root, text="Press enter to start",
+                              font=('Helvetica', 12))
+redscoreLabel.pack()
+
+scoreinstructions = tkinter.Label(root, text="Green score is on the top\nred score is on bottom")
+scoreinstructions.pack()
+# add a time left label
+timeLabel = tkinter.Label(root, text="Time left: " +
+                          str(timeleft), font=('Helvetica', 12))
+
 timeLabel.pack()
 
-label = tkinter.Label(root, font = ('Helvetica', 60))
+# add a label for displaying the colours
+label = tkinter.Label(root, font=('Helvetica', 60))
 label.pack()
 
+# add a text entry box for
+# typing in colours
 e = tkinter.Entry(root)
-
+x = tkinter.Entry(root)
+# run the 'startGame' function
+# when the enter key is pressed
 root.bind('<Return>', startGame)
 e.pack()
-
+x.pack()
+# set focus on the entry box
+x.focus_set()
 e.focus_set()
-
 root.mainloop()
