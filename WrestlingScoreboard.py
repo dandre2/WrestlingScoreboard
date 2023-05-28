dc29be1f09a6ded123dcdcaa5bf8a602dba0bb04
dandre2 <roman.adamsv2@gmail.com>
2023-05-28 09:44:54 -0500

    submission2

--- WrestlingScoreboard.py
+++ WrestlingScoreboard.py
@@ -4,8 +4,10 @@ import random
 
 scores = ['0', '1', '2', '3', '4',
           '5', '-5', '-4', '-3', '-2', '-1']
+time = ['1', '2', '3', '4', '5', '6']
 greenscore = 0
 redscore = 0
+period=0
 
 timeleft = 90
 
@@ -24,11 +26,14 @@ def nextColour():
     global redscore
     global greenscore
     global timeleft
+    global period
 
+    
     if timeleft > 0:
-
+        
         e.focus_set()
         x.focus_set()
+        p.focus_set()
 
         if e.get().lower() == scores[1]:
 
@@ -72,12 +77,29 @@ def nextColour():
              redscore -= 4
         if x.get().lower() == scores[-5]:
              redscore -= 5    
+        if p.get().lower() == scores[1]:
+             period +=1
+        if s.get().lower() == scores[1]:
+             timeleft +=1
+        if s.get().lower() == scores[2]:
+             timeleft +=2
+        if s.get().lower() == scores [3]:
+             timeleft +=3                 
+        if s.get().lower() == scores [4]:
+             timeleft +=4
+        if s.get().lower() == scores[5]:
+             timeleft +=5
+        if s.get().lower() == scores[6]:
+             timeleft +=6               
         e.delete(0, tkinter.END)
         x.delete(0, tkinter.END)
+        p.delete(0, tkinter.END)
+        s.delete(0, tkinter.END)
 
-        greenscoreLabel.config(text="green Score: " + str(greenscore))
+        greenscoreLabel.config(text="Green Score: " + str(greenscore))
 
-        redscoreLabel.config(text="redscore: " + str(redscore))
+        redscoreLabel.config(text="Red Score: " + str(redscore) + "Period:" + str(period))
+
 
 # Countdown timer function
 
@@ -121,12 +143,11 @@ greenscoreLabel = tkinter.Label(root, text="Press enter to start",
                                 font=('Helvetica', 12))
 greenscoreLabel.pack()
 
-redscoreLabel = tkinter.Label(root, text="Press enter to start",
+redscoreLabel = tkinter.Label(root, text=(period),
                               font=('Helvetica', 12))
 redscoreLabel.pack()
 
-scoreinstructions = tkinter.Label(root, text="Green score is on the top\nred score is on bottom")
-scoreinstructions.pack()
+
 # add a time left label
 timeLabel = tkinter.Label(root, text="Time left: " +
                           str(timeleft), font=('Helvetica', 12))
@@ -139,14 +160,27 @@ label.pack()
 
 # add a text entry box for
 # typing in colours
-e = tkinter.Entry(root)
-x = tkinter.Entry(root)
+v = tkinter.StringVar(root, value='Green score')
+d= tkinter.StringVar(root, value='red score')
+a= tkinter.StringVar(root, value='period')
+t= tkinter.StringVar(root, value='timeleft')
+e = tkinter.Entry(root, textvariable= v)
+x = tkinter.Entry(root, textvariable=d)
+p = tkinter.Entry(root, textvariable=a)
+s = tkinter.Entry(root, textvariable=t)
 # run the 'startGame' function
 # when the enter key is pressed
 root.bind('<Return>', startGame)
 e.pack()
 x.pack()
+p.pack()
+s.pack()
+someLabel = tkinter.Label(root, text="SomeLabel",
+                              font=('Helvetica', 12))
+someLabel.pack()
 # set focus on the entry box
 x.focus_set()
 e.focus_set()
+p.focus_set()
+s.focus_set()
 root.mainloop()
