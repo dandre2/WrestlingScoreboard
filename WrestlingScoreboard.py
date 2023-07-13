5142c8e3b29f07bc4c02b86aa87edd5d8f256309
dandre2 <roman.adamsv2@gmail.com>
2023-07-13 17:33:57 -0500

    stuck on stopping clock stil but using others ideas

--- WrestlingScoreboard.py
+++ WrestlingScoreboard.py
@@ -1,107 +1,112 @@
 import tkinter
 import time
-# Pausing time
-# starting time again in a new period
-# adding logic to stop time if theres a winner
+    # Pausing time
+    # starting time again in a new period
+    # adding logic to stop time if theres a winner
+
 
 def nextPeriod():
-    overtime1()
-    period['text'] += 1
+        overtime1()
+        period['text'] += 1
 
 def redScoreTwo():
-    redscore['text'] += 2
-    redScoreCheck()
+        redscore['text'] += 2
+        redScoreCheck()
 
 def greenScoreTwo():
-    greenscore['text'] += 2
-    greenScoreCheck()
+        greenscore['text'] += 2
+        greenScoreCheck()
 
 def redScoreOne():
-    redscore['text'] += 1
-    redScoreCheck()
+        redscore['text'] += 1
+        redScoreCheck()
 
 def greenScoreOne():
-    greenscore['text'] += 1
-    greenScoreCheck()
+        greenscore['text'] += 1
+        greenScoreCheck()
 
 
 def redScoreThree():
-    redscore['text'] += 3
-    redScoreCheck()
+        redscore['text'] += 3
+        redScoreCheck()
 
 def RedOt1win():
-    resultLabel['text']='red wins ot1'
+        resultLabel['text']='red wins ot1'
 
 def GreenScoreThree():
-    greenscore['text'] += 3
-    greenScoreCheck()
+        greenscore['text'] += 3
+        greenScoreCheck()
 
 def pinred():
-    resultLabel['text']='red wins by pin'
+        resultLabel['text']='red wins by pin'
 
 def pingreen():
-    resultLabel['text']='green wins by pin'    
-    
+        resultLabel['text']='green wins by pin'    
+        
 def overtime1():
-    if period['text']==3:
-        overtimeLabel['text']='first overtime, first takedown wins.'    
+        if period['text']==3:
+            overtimeLabel['text']='first overtime, first takedown wins.'    
 
 def greenScoreCheck():
-    if greenscore['text']>=15:
-        resultLabel['text']='green wins by techfall'
+        if greenscore['text']>=15:
+            resultLabel['text']='green wins by techfall'
 
 def redScoreCheck():
-    if redscore['text']>=15:
-        resultLabel['text']='red wins by techfall'
+        if redscore['text']>=15:
+            resultLabel['text']='red wins by techfall'
 
 
 scores = ['0', '1', '2', '3', '4',
-          '5', '-5', '-4', '-3', '-2', '-1']
+            '5', '-5', '-4', '-3', '-2', '-1']
 
 
 timeleft = 90
 
 
 def startGame(event):
-    if timeleft == 90:
-        #countdown()
+        if timeleft == 90:
+            #countdown()
 
-        nextColour()
-
+            nextColour()
+            # App(tkinter.Tk)
 
 def nextColour():
-    global redscore
-    global greenscore
-    global timeleft
+        global redscore
+        global greenscore
+        global timeleft
 
-    if timeleft > 0:
-        pass
-
-
-
-  
+        if timeleft > 0:
+            pass
 
 
-def countdown():
-    global timeleft
+# class App(tkinter.Tk):
+#         def __init__(self):
+#             super().__init__()
+#             self.title("Counting Seconds")
+#             self.counter = 90
+#             self.active_counter = False
+#             self.label = tkinter.Label(self, fg="black")
+#             self.label.pack()
+#             self.button = tkinter.Button(self, text='Start', width=50, command=self.start_stop)
+#             self.button.pack()
 
-    if timeleft > 0:
-        timeleft -= 1
-        
-        timeLabel.config(text="Time left: "
-                              + str(timeleft))
+        def count(self):
+            if self.active_counter:
+                self.counter -= 1
+                self.label.config(text=self.counter)
+                self.label.after(1000, self.count)
 
-        timeLabel.after(1000, countdown)
-def StopClock():
-    global time
-    if countdown ==False:    
-        time.sleep()
-    elif countdown ==True:
-        pass
-     
-        
+        def start_stop(self):
+            if self.button['text'] == 'Start':
+                self.active_counter = True
+                self.count()
+                self.button.config(text="Stop")
+            else:
+                self.active_counter = False
+                self.button.config(text="Start")
 
-        
+
+            
 
 root = tkinter.Tk()
 
@@ -109,9 +114,6 @@ root.title("COLORGAME")
 
 root.geometry("500x800")
 
-instructions = tkinter.Label(root, text="press enter to start timer",
-                             font=('Helvetica', 32))
-instructions.pack()
 
 greenLabel = tkinter.Label(root, text="Green Score")
 greenLabel.pack()
@@ -128,10 +130,33 @@ periodLabel.pack()
 period = tkinter.Label(root, text=1)
 period.pack()
 
-timeLabel = tkinter.Label(root, text="Time left: " +
-                                     str(timeleft), font=('Helvetica', 12))
+    
 
-timeLabel.pack()
+
+
+def start_stop(self):
+            if button['text'] == 'Start':
+                active_counter = True
+                count()
+                button.config(text="Stop")
+            else:
+                active_counter = False
+                button.config(text="Start")
+
+print("Counting Seconds")
+
+counter = 90
+active_counter = False
+label = tkinter.Label(root, fg="black")
+label.pack()
+button = tkinter.Button(root, text='Start', width=50, command=start_stop)
+button.pack()
+
+def count(self):
+    if active_counter:
+        counter -= 1
+        label.config(text=counter)
+        label.after(1000,count)
 
 resultLabel = tkinter.Label(root, text="empty")
 resultLabel.pack()
@@ -165,14 +190,15 @@ greenpinButton.pack()
 RedWinsOvertime1= tkinter.Button(root, text="red wins Ot1", command=RedOt1win)
 RedWinsOvertime1.pack()
 
-startclockbutton= tkinter.Button(root, text="start clock", command=countdown)
-startclockbutton.pack()
 
-StopClockbutton= tkinter.Button(root, text="to stop clock", command=StopClock)
-StopClockbutton.pack()
+
+    # StopClockbutton= tkinter.Button(root, text="to stop clock", command)
+    # StopClockbutton.pack()
 
 NextPeriodButton = tkinter.Button(root, text="Next Period", command=nextPeriod)
 NextPeriodButton.pack()
-
-root.bind('<Return>', startGame)
-root.mainloop()
+if __name__ == "__main__":
+        # root.bind(App().mainloop())
+        root.bind('<Return>', startGame)
+        root.mainloop()
+        
